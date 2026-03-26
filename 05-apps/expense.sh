775c805d41a6929219b2e225824ff184b699d8d8
ushanayana <ushanayana@gmail.com>
2026-03-26 14:52:03 +0530

    project

--- 05-apps/expense.sh
+++ 05-apps/expense.sh
@@ -2,7 +2,7 @@
 # user data will get sudo access
 dnf install ansible -y
 cd /tmp
-git clone https://github.com/daws-78s/expense-ansible-roles.git
+git clone https://github.com/ushanayana/expense-ansible-roles.git
 cd expense-ansible-roles
 ansible-playbook main.yaml -e component=backend -e login_password=ExpenseApp1
 ansible-playbook main.yaml -e component=frontend
