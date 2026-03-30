543d2ddfa605c3b085d7fb6e3ea1bf13dd1b24c8
ushanayana <ushanayana@gmail.com>
2026-03-30 11:07:58 +0530

    project

--- 05-apps/expense.sh
+++ 05-apps/expense.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # user data will get sudo access
 dnf install ansible -y
-cd /tmp
-git clone https://github.com/ushanayana/expense-ansible-roles.git
-cd expense-ansible-roles
-ansible-playbook main.yaml -e component=backend -e login_password=ExpenseApp1
-ansible-playbook main.yaml -e component=frontend
+ansible-pull  -i localhost, -U https://github.com/ushanayana/expense-ansible-roles.git main.yaml -e component=backend 
+# cd /tmp
+# git clone https://github.com/ushanayana/expense-ansible-roles.git
+# cd expense-ansible-roles
+# ansible-playbook main.yaml -e component=backend -e login_password=ExpenseApp1
+# ansible-playbook main.yaml -e component=frontend
