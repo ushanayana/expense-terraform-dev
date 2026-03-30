fc93f62c168669b1c2a75ebc4d4a390abfb98a49
ushanayana <ushanayana@gmail.com>
2026-03-30 20:05:23 +0530

    project

--- 05-apps/provider.tf
+++ 05-apps/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "6.33.0" # Terraform AWS provider version
+      version = "6.37" # Terraform AWS provider version
     }
   }
 
