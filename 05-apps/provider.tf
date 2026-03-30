9220c329255fc2c79891a5916d3c8ca51ac88d14
ushanayana <ushanayana@gmail.com>
2026-03-30 19:18:35 +0530

    message

--- 05-apps/provider.tf
+++ 05-apps/provider.tf
@@ -2,22 +2,42 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "6.37"
+      version = "6.33.0" # Terraform AWS provider version
     }
   }
 
-  
- backend "s3" {
-   bucket = "78s-remote-state"
-   key = "expense-dev-apps"
-   region = "us-east-1"
-   dynamodb_table = "daws78s-locking"
-   
- }
-
-  
+  backend "s3" {
+    bucket  = "78s-remote-state" # Replace with your unique bucket name
+    key     = "expense-dev-apps"
+    region  = "us-east-1"
+    encrypt = true
+    use_lockfile   = true
+  }
 }
 
 provider "aws" {
   region = "us-east-1"
 }
+# terraform {
+#   required_providers {
+#     aws = {
+#       source = "hashicorp/aws"
+#       version = "6.37"
+#     }
+#   }
+
+  
+#  backend "s3" {
+#    bucket = "78s-remote-state"
+#    key = "expense-dev-apps"
+#    region = "us-east-1"
+#    dynamodb_table = "daws78s-locking"
+   
+#  }
+
+  
+# }
+
+# provider "aws" {
+#   region = "us-east-1"
+# }
